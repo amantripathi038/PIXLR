8b18be48319997c0722f3d18a3d26b586463bd7d
amantripathi038 <84669840+amantripathi038@users.noreply.github.com>
2021-11-20 16:58:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var image = null;
 var can1 = null,can2 = null;
+var grayImage = null;
+var sepiaImage = null;
+var brightImage = null;
 function upload()
 {
     can1 = document.getElementById("can1");
-    var fileinput = document.getElementById("imgInput");
-    image = new SimpleImage(fileinput);
-    grayImage = new SimpleImage(fileinput);
+    var fileInput = document.getElementById("imgInput");
+    image = new SimpleImage(fileInput);
+    grayImage = new SimpleImage(fileInput);
+    sepiaImage = new SimpleImage(fileInput);
+    brightImage = new SimpleImage(fileInput);
     image.drawTo(can1);
 }
 function download()
@@ -43,9 +48,11 @@ function doGray()
     else
     {
         window.alert("Upload Image First");
+        return;
     }
     can2 = document.getElementById("can2");
     grayImage.drawTo(can2);
+    brightImage = grayImage;
 }
 function ensureInImage (coordinate, size) 
 {
@@ -69,9 +76,9 @@ function getPixelNearby (image, x, y, diameter)
 }
 function doBlur()
 {
-    var output = new SimpleImage(image.getWidth(), image.getHeight());
     if(image != null)
-    { 
+    {
+        var output = new SimpleImage(image.getWidth(), image.getHeight()); 
         for (var pixel of image.values())
         {
             var x = pixel.getX();
@@ -90,7 +97,104 @@ function doBlur()
     else
     {
         window.alert("Upload Image First");
+        return;
     }
     can2 = document.getElementById("can2");
-    output.drawTo(can2);    
+    output.drawTo(can2);
+    brightImage = output;    
+}
+function doSepia()
+{
+    if(sepiaImage != null)
+    { 
+        for (var pixel of sepiaImage.values())
+        {
+            var R = pixel.getRed();
+            var G = pixel.getGreen();
+            var B = pixel.getBlue();
+            var tr = 0.393*R + 0.769*G + 0.189*B;
+            var tg = 0.349*R + 0.686*G + 0.168*B;
+            var tb = 0.272*R + 0.534*G + 0.131*B;
+            if (tr>255)
+                pixel.setRed(255);
+            else
+                pixel.setRed(tr);
+            if (tg>255)
+                pixel.setGreen(255);
+            else
+                pixel.setGreen(tg);
+            if (tb>255)
+                pixel.setBlue(255);
+            else
+                pixel.setBlue(tb);
+        }    
+    }
+    else
+    {
+        window.alert("Upload Image First");
+        return;
+    }
+    can2 = document.getElementById("can2");
+    sepiaImage.drawTo(can2);
+    brightImage = sepiaImage;    
+}
+
+function doBright()
+{
+    if(image == null)
+    {
+        window.alert("Upload Image First");
+        return;
+    }
+    can2 = document.getElementById("can2");
+    image.drawTo(can2);
+    var x = document.getElementById("myRange");
+    var y = document.getElementById("myRange2");
+    var out = document.getElementById("value")
+    if (x.style.display === "none" && y.style.display === "none")
+    {
+        x.style.display = "block";
+        y.style.display = "inline-block";
+    }
+    else
+    {
+        x.style.display = "none";
+        y.style.display = "none";
+    }
+    out.innerHTML = x.value;
+    x.oninput = function() 
+    {
+        out.innerHTML = this.value;       
+    }
+}
+function Bright()
+{
+    var x = document.getElementById("myRange").value;
+    x = parseInt(x);
+    brightImage = new SimpleImage(image);
+    for (var pixel of brightImage.values())
+    {
+        var R = pixel.getRed() + x;
+        var B = pixel.getBlue() + x;
+        var G = pixel.getGreen() + x;
+        if(R>255)
+            pixel.setRed(255);
+        else if (R<0)
+            pixel.setRed(0);
+        else
+            pixel.setRed(R);
+        if (G>255)
+            pixel.setGreen(255);
+        else if (G<0)
+            pixel.setGreen(0);
+        else 
+            pixel.setGreen(G);
+        if(B>255)
+            pixel.setBlue(255);
+        else if (B<0)
+            pixel.setBlue(0);
+        else 
+            pixel.setBlue(B);
+    }
+    brightImage.drawTo(can2);
 }
